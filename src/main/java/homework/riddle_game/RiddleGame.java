dfcd07150a007e20341f8f3b4691853b059d79f8
Светлана Бурдуковская <svetlanaburdukovskaa@Svetlanas-MacBook-Pro.local>
2020-09-02 09:13:06 +0300

    homework

--- src/main/java/homework/riddle_game/RiddleGame.java
+++ src/main/java/homework/riddle_game/RiddleGame.java
@@ -1,7 +1,7 @@
 package homework.riddle_game;
 
 import design_patterns.template_method.Game;
-
+import java.util.Scanner;
 import javax.swing.*;
 
 /**
@@ -12,24 +12,42 @@ public class RiddleGame implements Game {
     //todo
 
     private final int maxLimit;
-
     public RiddleGame(int max) {
         this.maxLimit = max;
-
     }
 
     public static void main(String[] args) {
 
-        int s = Integer.parseInt(JOptionPane.showInputDialog("input your number"));
-        System.out.println(s);
+        int maxLimit = Integer.parseInt(JOptionPane.showInputDialog("input maximum number"));
+        System.out.println(maxLimit);
+        RiddleGame game = new RiddleGame(maxLimit);
+        game.play();
+
+
     }
-
-
-
-
-
+    public int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
     @Override
     public void play() {
+        int s = Integer.parseInt(JOptionPane.showInputDialog("input your number"));
+        int suggestNumber = getRandomNumber(0, maxLimit);
+        System.out.println(suggestNumber);
+        while(true){
+            if (s < suggestNumber){
+                System.out.format("The suggestion number is more than %d \n",  s);
+            }
+            if (s > suggestNumber){
+                System.out.format("The suggestion number is less than %d \n",  s);
+            }
+            if (s == suggestNumber){
+                System.out.format("You win!!! \n");
+                System.out.format("Suggested number is %d \n", s);
+                break;
+            }
+            s = Integer.parseInt(JOptionPane.showInputDialog("input your number"));
+
+        }
 
     }
 }
